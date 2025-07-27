cd5cbde8e0d65380a9e049c342f2bce47a116af6
José Jorge <74927286+jorgehauck@users.noreply.github.com>
2025-07-27 17:29:55 -0300

    Feat: Formulário de clientes.

--- front-end/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
+++ front-end/clientes-app/src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente } from '../model/cliente';
+
 @Component({
   selector: 'app-clientes-form',
   templateUrl: './clientes-form.component.html',
-  styleUrls: ['./clientes-form.component.css']
+  styleUrls: ['./clientes-form.component.css'],
 })
 export class ClientesFormComponent implements OnInit {
+  public cliente: Cliente;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
